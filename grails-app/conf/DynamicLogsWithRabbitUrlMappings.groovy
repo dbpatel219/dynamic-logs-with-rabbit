9c341698e9308825e4470e60800c12551ac6977a
Dharmesh Patel <dbpatel@findexp.com>
2014-12-08 16:33:03 -0500

    Rename classes to incorporate the new plugin name.

--- grails-app/conf/DynamicLogsWithRabbitUrlMappings.groovy
+++ grails-app/conf/DynamicLogsWithRabbitUrlMappings.groovy
@@ -2,7 +2,7 @@ class DynamicLoggingUrlMappings {
 
     static mappings = { applicationContext ->
         // By Default the controller will be disabled
-        if (!applicationContext.grailsApplication.config.dynamiclogging.enable.logLevelController) {
+        if (! applicationContext.grailsApplication.config.dynamiclogging.enable.logLevelController) {
             "/logLevel/$action?"(view: '/error')
         }
     }
