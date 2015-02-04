d860a8042a62aa2bf60cdebcbfcb939ebcf4afcb
Dharmesh Patel <dbpatel@findexp.com>
2015-02-04 04:24:30 -0500

    Handle msg actions for listPlugins and changeLogLevels

--- grails-app/services/grails/plugin/dynamiclogs/LogLevelListenerService.groovy
+++ grails-app/services/grails/plugin/dynamiclogs/LogLevelListenerService.groovy
@@ -12,20 +12,43 @@ class LogLevelListenerService {
 
     private final String currentAppName = Metadata.current.getApplicationName()
 
+    def pluginManager
+
     void handleMessage(message) {
-        log.info("Recieved Log Level change message - ${message}")
+        log.info("Received Dynamic Logs change message - ${message}")
         def msg = JSON.parse(message)
 
         if (msg.appName != currentAppName) {
             return
         }
 
-        def logLevel = Level.toLevel(msg.logLevel)
-        def logger = LogManager.getLogger(msg.loggerName)
+        def action = msg.msgAction
+        switch (action) {
+            case 'listPlugins':
+                listPlugins(msg)
+                break
+            case 'changeLogLevel':
+                changeLogLevel(msg)
+                break
+            default :
+                log.warn("Dynamic Log Level $action NOT supported.")
+        }
+    }
 
-        if (logger && logLevel) {
-            log.info("Changing Log Level $msg.loggerName --> $logLevel")
-            logger.level = logLevel
+    private listPlugins(msg) {
+        def plugins = pluginManager.allPlugins
+        log.info("Found ${plugins?.size()} plugins")
+        log.info(plugins.toString())
+    }
+
+    private changeLogLevel(msg) {
+        if (msg.logLevel && msg.loggerName) {
+            def logLevel = Level.toLevel(msg?.logLevel)
+            def logger = LogManager.getLogger(msg?.loggerName)
+            if (logger && logLevel) {
+                log.info("Changing Log Level $msg.loggerName --> $logLevel")
+                logger.level = logLevel
+            }
         } else {
             log.error("Either loggerName or logLevel was not a valid input.  $msg")
         }
