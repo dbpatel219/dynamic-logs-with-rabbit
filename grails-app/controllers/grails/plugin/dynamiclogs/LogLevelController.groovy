a08a7b0935df686d3b6f2c5002d9ca9ff94f1b0e
Dharmesh Patel <dbpatel@findexp.com>
2015-02-04 04:23:57 -0500

    Add msgAction field to model

--- grails-app/controllers/grails/plugin/dynamiclogs/LogLevelController.groovy
+++ grails-app/controllers/grails/plugin/dynamiclogs/LogLevelController.groovy
@@ -4,21 +4,21 @@ import grails.util.Metadata
 
 class LogLevelController {
 
-    private static final List<String> LOG_LEVEL_NAMES = ['ALL','DEBUG','ERROR','FATAL','INFO','OFF','TRACE','WARN']
+    private static final List<String> ACTION_LIST = ['changeLogLevel', 'listPlugins']
     private static final List<String> LOG_LEVEL_NAMES = ['OFF','FATAL','ERROR','WARN','INFO','DEBUG','TRACE','ALL']
     private static final String APP_NAME = Metadata.current.getApplicationName()
 
     def logLevelService
 
     def index() {
-        [cmd: new DynamicLogLevelMsg(), applicationName: APP_NAME, logLevelNames: LOG_LEVEL_NAMES]
+        [cmd: new DynamicLogLevelMsg(), applicationName: APP_NAME, logLevelNames: LOG_LEVEL_NAMES, actionList: ACTION_LIST]
     }
 
     def changeLogLevel(DynamicLogLevelMsg cmd) {
         logLevelService.send(cmd)
 
-        flash.message = "Log Level Change message sent: $cmd"
+        flash.message = "Message sent: $cmd"
 
-        render(view: "index", model: [cmd: cmd, applicationName: APP_NAME, logLevelNames: LOG_LEVEL_NAMES])
+        render(view: "index", model: [cmd: cmd, applicationName: APP_NAME, logLevelNames: LOG_LEVEL_NAMES, actionList: ACTION_LIST])
     }
 }
