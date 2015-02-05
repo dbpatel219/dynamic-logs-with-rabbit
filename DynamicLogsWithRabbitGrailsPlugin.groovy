e93dfa6896a7d8bdaa8622d8bbf89b71d3573839
Dharmesh Patel <dbpatel@findexp.com>
2015-02-05 13:16:05 -0500

    Add loadAfter services requirement

--- DynamicLogsWithRabbitGrailsPlugin.groovy
+++ DynamicLogsWithRabbitGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 import grails.util.Environment
 
 class DynamicLogsWithRabbitGrailsPlugin {
-    def version = "0.4.2"
+    def version = "0.4.3"
     def grailsVersion = "2.1 > *"
     def title = "Dynamic Logs With Rabbit Plugin"
     def description = 'Allows you to dynamically change log levels on all instances listening to a particular topic via RabbitMQ Messages.'
@@ -12,6 +12,7 @@ class DynamicLogsWithRabbitGrailsPlugin {
     def issueManagement = [system: "GITHUB", url: "https://github.com/dbpfindexp/dynamic-logs-with-rabbit/issues"]
     def scm = [ url: "https://github.com/dbpfindexp/dynamic-logs-with-rabbit" ]
 
+    def loadAfter = ['services']
     def loadBefore = ['rabbitmq']
 
     def doWithSpring = {
